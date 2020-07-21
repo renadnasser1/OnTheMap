e6edb98278909bd1e14ebc25705346d5b9bb2c64
renadnasser1 <56361699+renadnasser1@users.noreply.github.com>
2020-07-21 21:18:40 +0300

    Error Handller

---
+++ OnTheMap/Controller/ErrorHandller.swift
@@ -0,0 +1,24 @@
+//
+//  ErrorHandller.swift
+//  OnTheMap
+//
+//  Created by Renad nasser on 21/07/2020.
+//  Copyright © 2020 Renad nasser. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ErrorHandller: UIAlertController{
+    
+    class func showAlert(title:String?, message:String, viewController:UIViewController){
+        
+        let myAlert = UIAlertController(title: title ?? "", message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        myAlert.addAction(okAction)
+        
+        viewController.present(myAlert, animated: true, completion: nil)
+        
+    }
+}
